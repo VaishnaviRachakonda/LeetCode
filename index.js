3ab801178d69c05737c4a36ea971aefc9539072a
Rachakonda Sai <72305047+VaishnaviRachakonda@users.noreply.github.com>
2022-02-21 12:00:53 +0000

    Final answer uses hash map

--- index.js
+++ index.js
@@ -3,17 +3,19 @@
  * @return {number}
  */
 var majorityElement = function(nums) {
-    let map =new Map();
-    for(let i=0;i<nums.length;i++){
-        if(map.has(nums[i])){
-            map.set(nums[i],map.get(nums[i]+1));
-            if(map.get(nums[i])>Math.ceil(nums.length/2)){
-                return nums[i]
-            }
-        }
-        else{
-            map.set(nums[i],1);
-        }
+  if (nums.length === 1) return nums[0]
+  let map = new Map();
+  for (let i = 0; i < nums.length; i++) {
+    if (map.has(nums[i])) {
+      map.set(nums[i], map.get(nums[i]) + 1);
+      if (map.get(nums[i]) >= nums.length / 2) {
+        return nums[i]
+      }
     }
+    else {
+      map.set(nums[i], 1);
+    }
+  }
 };
-console.log('1',)
+console.log('1', majorityElement([3, 2, 3]))
+console.log('2', majorityElement([1]))
